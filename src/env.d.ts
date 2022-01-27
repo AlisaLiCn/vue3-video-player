5c0c6a2c01543a9f3e889d2ac2896bdb71d76fbf
lishuang5 <lishuang5@soyoung.com>
2022-01-27 15:42:28 +0800

    update declare file

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'video.js' {
+  import videojs from 'video.js'
+  export default videojs
+}
